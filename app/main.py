3cfda8935c55eda94577566a82ddedf0b6653b0c
shewa420 <shewa123456789@gmail.com>
2025-05-10 15:24:41 +0300

    new changes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from . import models, database
 from .auth import router as auth_router
 from .admin import router as admin_router
@@ -6,5 +7,10 @@ from .admin import router as admin_router
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+# Підключаємо API
 app.include_router(auth_router)
 app.include_router(admin_router)
+
+# Статичні HTML файли
+app.mount("/", StaticFiles(directory="app/static/frontend", html=True), name="static")
