db7b52c8c58488f025b274d7d45637d0fd0631ec
shewa420 <shewa123456789@gmail.com>
2025-06-04 20:29:10 +0300

    глобальні зміни х2

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,19 @@
 from flask import Flask
-from . import models
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    db.init_app(app)
+    app.config.from_object("app.config.Config")
 
-    with app.app_context():
-        db.create_all()
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .models import User
+    from .routes import main
+    app.register_blueprint(main)
 
     return app
-
-from flask_login import LoginManager
-
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
-
-from .models import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
